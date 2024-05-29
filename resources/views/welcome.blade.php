3fb7d57b96e7288eb9bf55d71e88be6741a5a9f9
Ray <patrickrayymnd@students.unnes.ac.id>
2024-05-29 18:02:33 +0700

    Update coba

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>PALU</title>
+        <title>PALU RPL</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
