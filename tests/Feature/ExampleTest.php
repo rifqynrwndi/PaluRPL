6c733b13e4a25596ec017db9d17124579ea10da2
Ray <patrickrayymnd@students.unnes.ac.id>
2024-05-29 21:06:55 +0700

    "Updated ExampleTest.php files in tests/Feature and tests/Unit directories: added use statement, renamed test methods, and added return type declarations."

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -2,18 +2,19 @@
 
 namespace Tests\Feature;
 
-// use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
     /**
      * A basic test example.
-     */
-    public function test_the_application_returns_a_successful_response(): void
+     *
+     * @return void
+    */
+    public function test_example()
     {
         $response = $this->get('/');
-
         $response->assertStatus(200);
     }
 }
